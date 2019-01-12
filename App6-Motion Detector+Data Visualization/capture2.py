34326d93f96a320daeebb7326881a2a41e3c7101
Yuchen <378028556@qq.com>
2019-01-11 19:39:48 -0500

    Add files via upload

---
+++ App6-Motion Detector+Data Visualization/capture2.py
@@ -0,0 +1,37 @@
+import cv2,time
+
+first_frame=None
+
+video=cv2.VideoCapture(0)
+
+count=0
+while True:
+
+    check,frame=video.read()
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray,(21,21),0)
+
+    # if count<100:
+    #     first_frame=gray
+    #     count+=1
+    #     continue
+    if first_frame is None:
+        first_frame=gray
+        continue
+
+    delta_frame=cv2.absdiff(first_frame,gray)
+
+
+
+    cv2.imshow("Gray Frame",gray)
+    cv2.imshow("Delta Frame",delta_frame)
+
+    key=cv2.waitKey(1)
+    print(gray)
+
+    if key==ord('q'):
+        break
+
+video.release()
+cv2.destroyALLWindows
